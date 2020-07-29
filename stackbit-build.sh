67b728067a103aad7d603921328d033c44e1a154
stackbit-projects <projects@stackbit.com>
2020-07-29 23:35:20 +0300

    Update configuration

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,12 +4,12 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/pull > /dev/null
+curl -s -X POST https://artem-stackbit.ngrok.io/project/5f21dd81f80a355c42b75bb8/webhook/build/pull > /dev/null
 if [[ -z "${STACKBIT_API_KEY}" ]]; then
     echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
 else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
+    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://artem-stackbit.ngrok.io/pull/5f21dd81f80a355c42b75bb8
 fi
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://artem-stackbit.ngrok.io/project/5f21dd81f80a355c42b75bb8/webhook/build/ssgbuild > /dev/null
 gatsby build
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://artem-stackbit.ngrok.io/project/5f21dd81f80a355c42b75bb8/webhook/build/publish > /dev/null
